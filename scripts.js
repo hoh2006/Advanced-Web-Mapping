7c9091a05e88c9545ce6619282582c738f28aa4a
hoh2006 <hoh2006@nyu.edu>
2025-03-20 19:31:25 -0400

    set up website with html, css, and js files

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function injectImage() {
+    const img = document.createElement("img");
+
+    img.src =
+        "https://www.lakemerritt.org/uploads/7/7/2/9/7729843/imgp2225.jpg";
+
+    img.style.width = "500px";
+
+    const container = document.getElementById("bird-appears");
+
+    container.innerHTML = '';
+    container.appendChild(img)
+}
